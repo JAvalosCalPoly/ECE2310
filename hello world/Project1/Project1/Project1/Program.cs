a2631842c383ed46edd6997b275811b92610d0ac
Jonathan Avalos <166433757+JAvalosCalPoly@users.noreply.github.com>
2024-09-26 15:14:28 -0700

    Add files via upload

---
+++ hello world/Project1/Project1/Project1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("~~~~~~Welcome To Hypotenuse Solver~~~~~~\n");
+            Console.Write("Imput the first leg: ");            //Prompt
+            Double L1 = Double.Parse(Console.ReadLine());      //Reads and converts to double
+            Console.Write("Imput the second leg: ");           //Prompt
+            Double L2 = Double.Parse(Console.ReadLine());      //Reads and converts to double
+            Double LegSum = Math.Pow(L1 , 2) + Math.Pow(L2 , 2); //sums the squares of both variables
+            Double HypInter = Math.Sqrt(LegSum);                    //Square roots the sum
+            Double Hyp = Math.Round(HypInter,3);                 //Rounds to 3 places
+            Console.WriteLine("The Hypotenuse of your triangle is {0}.", Hyp);
+                //end
+                Console.Read();
+        }
+    }
+}
---
+++ hello world/Temperature Converter/TempConv/Temperature Converter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ptompt
+            Console.WriteLine("Hello, what is the temperature today in Fahrenheit?");
+            //read imput
+            String Dat = Console.ReadLine();
+            Double Temp = Convert.ToDouble( Dat );
+            //convert to celcius
+            double NTemp = (Temp - 32)* 5/9;
+            //round
+            Double RTemp = Math.Round(NTemp, 1);
+            //output
+            Console.WriteLine("That is {0} degrees Celcius", RTemp);
+            Console.Read();
+        }
+    }
+}
