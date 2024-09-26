781542085a363ad4311d855c5fd8c3b0bd5be3fa
Jonathan Avalos <166433757+JAvalosCalPoly@users.noreply.github.com>
2024-09-26 15:26:28 -0700

    Add files via upload

--- class ecercise 2/class ecercise 2/Program.cs
+++ class ecercise 2/class ecercise 2/Program.cs
@@ -10,30 +10,46 @@ namespace class_ecercise_2
     {
         static void Main(string[] args)
         {
-            //
-            ////Prompt and Validation for size of Class   
-            //Console.WriteLine("What is the size of the class?");
-            //bool Kver = Int32.TryParse(Console.ReadLine(), out int K);
-            ////loops until valid number is inputted
-            //while (!Kver || K <= 0)
-            //{
-            //    if (!Kver)
-            //        Console.WriteLine("Please enter a number...");
-            //    else
-            //        Console.WriteLine("please enter a positive number");
-            //    Console.WriteLine("What is the size of the class?");
-            //    Kver = Int32.TryParse(Console.ReadLine(), out K);
-            //}
-            //Console.WriteLine($"Class Size {K}.");
-            //
-            int K = 0;
-            bool Kver;
+
+            //Prompt and Validation for size of Class   
+            Console.WriteLine("What is the size of the class?");
+            Console.Write("--> ");
+            bool Kver = Int32.TryParse(Console.ReadLine(), out int K);
+            //loops until valid number is inputted
+            while (!Kver || K <= 0)
+            {
+                if (!Kver)
+                    Console.WriteLine("Please enter a number...");
+                else
+                    Console.WriteLine("please enter a positive number");
+                Console.WriteLine("What is the size of the class?");
+                Kver = Int32.TryParse(Console.ReadLine(), out K);
+            }
+            Console.WriteLine($"Class Size: {K}");
+            Console.WriteLine("________________________________");
             int Scor;
+            int Ave = 0;
+            int HSc = 0;
             Random nj = new Random();
-            Scor = nj.Next(0, 101);
-            Console.WriteLine($"This score is {Scor}");
-        
-        Console.Read();
+            //Assigns random grades
+            for (int i = 1; i <= K; i++)
+            {
+                Scor = nj.Next(60, 101);
+                Console.WriteLine($"The score of student #{i} is {Scor}");
+                Ave += Scor;
+                if (i == 1)
+                    HSc = Scor;
+                if (HSc < Scor)
+                    HSc = Scor;
+            }
+            //computes averages
+            Console.WriteLine("--------------------------------");
+            Ave /= K;
+            Console.WriteLine($"The average is {Ave}");
+            Console.WriteLine($"The highest score is {HSc}");
+            Console.WriteLine("--------------------------------");
+
+            Console.Read();
         }
     }
 }
