bafd6543fb32e08db9ebf1fe3a5bed999d2d9e92
Jonathan Avalos <166433757+JAvalosCalPoly@users.noreply.github.com>
2024-12-03 14:48:34 -0800

    Add files via upload

---
+++ Utilities/Utilities/Util.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Utilities
+{
+    public class Util
+    {
+        public static void GetInput(string item, double lower, ref double x)
+        {
+            bool valid;
+            do
+            {
+                Console.Write($"Please enter the {item}: ");
+                valid = double.TryParse(Console.ReadLine(), out x);
+                if (!valid || x < lower)
+                {
+                    Console.WriteLine("\nInvalid input, please try again.");
+                }
+            } while (!valid || x < lower);
+        }
+        public static void GetInput(string item, double lower, ref int x)
+        {
+            bool valid;
+            do
+            {
+                Console.Write($"Please enter the {item}: ");
+                valid = int.TryParse(Console.ReadLine(), out x);
+                if (!valid || x < lower)
+                {
+                    Console.WriteLine("\nInvalid input, please try again.");
+                }
+            } while (!valid || x < lower);
+        }
+        public static void GetInput(string item, double lower, double higher, ref double x)
+        {
+            bool valid;
+            do
+            {
+                Console.Write($"Please enter the {item}: ");
+                valid = double.TryParse(Console.ReadLine(), out x);
+                if (!valid || x < lower || x > higher)
+                {
+                    Console.WriteLine("\nInvalid input, please try again.");
+                }
+            } while (!valid || x < lower || x > higher);
+        }
+        public static T Highest<T>(T[] array) where T : IComparable<T> => array.Max();
+        public static T Lowest<T>(T[] array) where T : IComparable<T> => array.Min();
+    }
+}
